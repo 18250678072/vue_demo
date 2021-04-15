1900b5b37e13681e79285c5e4c0afec32dfd27b9
Youhaishan <2362785772@qq.com>
2021-04-16 01:35:38 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        {{ message }}
+        <span :title="message">
+            鼠标悬停几秒钟查看此处动态绑定的提示信息！
+        </span>
+        <ul>
+            <li v-for="(item, index) in arr" :key="index">{{ item }}</li>
+        </ul>
+        <input v-model="inputMsg" />
+        <p>{{ newInputMsg }}</p>
+        <p>{{ wathcMsg }}</p>
+        <p>{{ wathcMsg }}</p>
+        <p>{{ wathcMsg }}</p>
+        <hello-world :title="message" @appEvent="appEvent"></hello-world>
+        <button @click="test">反转消息</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+    data() {
+        return {
+            message: 'aaa',
+            arr: [1, 2, 3],
+            inputMsg: 'input',
+            wathcMsg: '',
+        };
+    },
+    methods: {
+        test() {
+            console.log('test');
+        },
+        appEvent(param) {
+            console.log('appEvent:', param);
+        },
+    },
+    computed: {
+        newInputMsg() {
+            console.log('computed');
+            return this.inputMsg + '1';
+        },
+    },
+    watch: {
+        inputMsg: function(val) {
+            console.log('watch');
+            this.wathcMsg = val + 2;
+        },
+    },
+    created() {
+        console.log('created');
+        this.test();
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
