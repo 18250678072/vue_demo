9f5bd5f2faef60ab749b06a26011e9b9ce049596
youhaishan <2362785772@qq.com>
2021-05-07 18:38:59 +0800

    vuex

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -2,8 +2,24 @@
   <div>
     <h1 class="title">homepage</h1>
     <h2>TOKEN: {{ token }}</h2>
+    <el-input v-model="token"></el-input>
+    <el-button type="primary" @click="modifyToken">modify token</el-button>
+    <el-button type="primary" @click="asyncModifyToken(token)"
+      >async modify token</el-button
+    >
+
     <el-button type="primary" @click="exit">退出</el-button>
-    <p>login count: {{ this.$store.state.count }}</p>
+    <p>login count: {{ this.$store.state.storeA.count }} - {{ count }}</p>
+    <p>storeB count: {{ this.$store.state.storeB.countB }} - {{ count }}</p>
+
+    <el-button type="primary" @click="addCount">add count</el-button>
+    <el-button type="primary" @click="addList">add list</el-button>
+
+    <ul>
+      <li v-for="(item, index) in filterList" :key="index">
+        {{ item }}
+      </li>
+    </ul>
     <div>
       <router-link to="/homepage/foo">foo</router-link>
     </div>
@@ -15,6 +31,7 @@
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from "vuex";
 export default {
   data() {
     return {
@@ -22,15 +39,36 @@ export default {
     };
   },
   methods: {
+    ...mapActions({
+      asyncModifyToken: "storeA/asyncSetToken",
+    }),
     exit() {
       localStorage.setItem("TOKEN", "");
       this.$router.replace({
         path: "/login",
       });
     },
+    addCount() {
+      this.$store.commit("increment");
+    },
+    modifyToken() {
+      // this.$store.commit("setToken", this.token);
+      this.$store.dispatch("storeA/asyncSetToken", this.token);
+    },
+    addList() {
+      this.$store.commit("addList", 3);
+    },
+  },
+  computed: {
+    ...mapGetters(["filterList"]),
+    ...mapState([
+      // 映射 this.count 为 store.state.count
+      "count",
+    ]),
   },
   created() {
     this.token = localStorage.getItem("TOKEN");
+    this.$store.commit("setToken", this.token);
     console.log(this.$store.state.count);
   },
 };
